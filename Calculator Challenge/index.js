1ed93a0a7d822327211e7cf5c36dadafab8dfca8
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-20 15:56:35 +0800

    Modulus and Exponent Function
    Added Modulus and Exponent functions on Calculator

--- Calculator Challenge/index.js
+++ Calculator Challenge/index.js
@@ -44,6 +44,7 @@ function exponent(){
     sumEl.textContent += result
 }
 
+
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
