a4f7d0f6173081eb64cf18d45ac1f972012ec991
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-05-20 12:43:42 +0800

    Create myGreeting.js
    // Create two variables name and greeting. The name variable should store your name and greeting eg. "Hi, my name is "
    
    //Create a third variable myGreeting that concatenates the two strings
    
    //Log myGreeting to the console

---
+++ myGreeting.js
@@ -0,0 +1,10 @@
+// Create two variables name and reeting. The name variable should store your name abd greeting eg. "Hi, my name is "
+
+//Create a third variable myGreeting that concatinates the two strings
+//Log myGreeting to the console
+
+let name = "Mark Jayson Rodelas"
+let greeting =  "Hi, my name is "
+let myGreeting = greeting + name + "!"
+
+console.log(myGreeting)
